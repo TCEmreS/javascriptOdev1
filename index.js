86627da78842ef2f66c8275cc050dfc7deadda82
Emre Soysal <90708341+TCEmreS@users.noreply.github.com>
2022-02-04 00:33:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const firmalar = [
+  "armani",
+  "boss",
+  "emporio",
+  "lacoste",
+  "champions",
+  "zara",
+  "h&m",
+  "lcw",
+  "kenzo",
+  "guess",
+  "guardini",
+  "noonBoot",
+  "valentino",
+  "moschino",
+  "etro",
+  "garage",
+  "defacto",
+  "exxe",
+  "koton",
+  "damat",
+  "suvari",
+];
+console.log(firmalar);
+
+const firmalar1 = {
+  adi: "armani",
+  kurulusyili: 5858,
+  sahibininAdi: "emre",
+  depatmanlar: {
+    ilkdepartman: "güvenlik",
+    ikincidepartman: "yazilim",
+    üçüncüdepartman: "temizlik",
+    dördüncüdepartman: "teknik",
+  },
+  ilkdepartman: [
+    { ad: "güvenlik" },
+    { ekiplideri: "emre" },
+    { calısanlar: ["omer", "reyhan"] },
+  ],
+  omer: { email: "omer@gmail.com", telefon: 123456789 },
+  reyhan: { email: "reyhan@gmail.com", telefon: 123456789 },
+};
+
+console.log(firmalar1);
